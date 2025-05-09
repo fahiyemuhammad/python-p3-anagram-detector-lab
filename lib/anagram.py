868c55aada2568dcb0cc88ca43ee4c9a7f6394ab
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-09 17:24:21 +0300

    Writing an anagram detector to check if a word is in a given list

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+    
+class Anagram:
+    def __init__(self,word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self,list):
+        return [
+            wrd for wrd in list
+            if sorted(wrd.lower()) == self.sorted_word and wrd.lower() != self.word
+        ]
+
+        
+    
